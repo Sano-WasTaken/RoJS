fda9c31d5c3a81f020d50e3ffce79d63372d22b5
Sano-WasTaken <darkrouille@gmail.com>
2024-02-04 23:09:19 +0100

    Add Express

--- src/index.ts
+++ src/index.ts
@@ -15,20 +15,14 @@ const ds = universe.getDataStore("Coins");
 
 //ds.listEntries().then(res => console.log(res.data.keys))
 
-async function test()
-{
-    const one = await listGroupMemberships(key, 15503263).then(res => res.data.groupMemberships[0].role)
-    const two = await listGroupRoles(key, 15503263).then(res => res.data.groupRoles[0].path)
-
-    return one === two
-}
-
-test()
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-
-console.error("error")
-
-getUniverseInformations(uniID).then(res => console.log(res.data.data))
-
 //listInventoryItem(key, 162206312).then(res => console.log(res.data.inventoryItems[0]))
+
+import e = require("express")
+
+const app = e()
+
+app.get("/", function(req, res){
+    res.send("test !dfgsqjkhkl")
+})
+
+app.listen(3000)
